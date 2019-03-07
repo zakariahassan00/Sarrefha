ce76a7d642ba364376986a0b8eb4cbb0a46acd0f
Zakaria Hassan <zakariahassan00@hotmail.com>
2019-03-07 04:29:53 +0200

    finished navbar, location dropwdown and the main grid

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+  let dropdown_header = $(".dropdown_header").html();
+  let chosenCity = "";
+
+  // toggle the content of the location bar
+  $(".dropdown_header").on("click", () => {
+    $(".location_dropDown").toggleClass("hidden");
+  });
+
+  // change the location
+  $(".city").on("click", function(e) {
+    e.preventDefault();
+    chosenCity = $(this).html();
+    $(".dropdown_header").html(`(${dropdown_header} (${chosenCity}`);
+    $(".location_dropDown").toggleClass("hidden");
+  });
+});
